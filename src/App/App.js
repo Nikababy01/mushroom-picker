a2313f6f9e166116cd4cd1fc13f6d48c25c15fc3
Monique.Bass01 <monique.bass01@gmail.com>
2020-05-14 12:17:15 -0500

    added cards

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import './App.scss';
 
+import mushroomData from '../helpers/data/mushroomData';
+import Forest from '../components/Forest/Forest';
+
+
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">Mushroom Picker</button>
-      </div>
-    );
+  state = {
+    mushrooms: [],
   }
+
+  componentDidMount() {
+    const mushrooms = mushroomData.getMushrooms();
+    console.error('mushrooms', mushrooms);
+    this.setState({ mushrooms });
+  }
+
+pickAMushroomBtn = () => {
+  mushroomData.pickAMushroom();
+  const mushrooms = mushroomData.getMushrooms();
+  const basket = mushroomData.getBasket();
+  this.setState({ mushrooms, basket });
+}
+
+pickAMushroomEvent = (e) => {
+  console.log('clicked mushroom picker');
+  e.preventDefault();
+  this.pickAMushroomBtn();
+}
+
+
+render() {
+  const { mushrooms } = this.state;
+  return (
+        <div className="App">
+          <h1>Mushroom Picker</h1>
+          <button className="btn btn-danger mb-2" onClick={this.pickAMushroomEvent}>Pick A Mushroom</button>
+          <Forest mushrooms={mushrooms}/>
+        </div>
+  );
+}
 }
 
 export default App;
